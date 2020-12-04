fc49d609ee92a3bf01174c778c5f643356bb7bda
liuyuxin-cloud <2590177089@qq.com>
2020-12-04 22:30:09 +0800

    12.7

--- Androidstart/QuizTest/app/src/main/java/com/example/geoquiz/MainActivity.java
+++ Androidstart/QuizTest/app/src/main/java/com/example/geoquiz/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.geoquiz;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -15,26 +17,29 @@ public class MainActivity extends AppCompatActivity {
     private Button mFalseButton;
     private Button mNextButton;
     private Button mPrevButton;
+    private Button mCheatButton;
     private TextView mQuestionTextView;
     private int mCurrentIndex = 0;
+    private boolean mIsCheater;
     private static final String TAG = "MainActivity";
     private static final String KEY_INDEX = "index";                //新增敞亮用来存储在bundle中键-值对中的键
+    private static final int REQUEST_CODE_CHEAT = 0;
     private Question[] mQuestionBank = new Question[]{              //设置数组用来存放问题
-            new Question(R.string.question_australia,true),
-            new Question(R.string.question_oceans,true),
-            new Question(R.string.question_mideast,false),
-            new Question(R.string.question_africa,false),
-            new Question(R.string.question_americas,true),
-            new Question(R.string.question_asia,true)
+            new Question(R.string.question_australia, true),
+            new Question(R.string.question_oceans, true),
+            new Question(R.string.question_mideast, false),
+            new Question(R.string.question_africa, false),
+            new Question(R.string.question_americas, true),
+            new Question(R.string.question_asia, true)
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {             //固定代码
         super.onCreate(savedInstanceState);
-        Log.d(TAG,"onCreate(Bundle) called");
+        Log.d(TAG, "onCreate(Bundle) called");
         setContentView(R.layout.activity_main);
-        if(savedInstanceState != null){                              //设置暂存区
-            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX,0);
+        if (savedInstanceState != null) {                              //设置暂存区
+            mCurrentIndex = savedInstanceState.getInt(KEY_INDEX, 0);
         }
 
         mQuestionTextView = (TextView) findViewById(R.id.question_text_view);    //将设置的文本框内容与资源相连
@@ -46,69 +51,99 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        mTrueButton = (Button) findViewById (R.id.true_button);      //设置所需按钮与监听器
-        mTrueButton.setOnClickListener (new View.OnClickListener(){
+        mTrueButton = (Button) findViewById(R.id.true_button);      //设置所需按钮与监听器
+        mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 checkAnswer(true);
             }
         });
-        mFalseButton = (Button) findViewById (R.id.false_button);
-        mFalseButton.setOnClickListener (new View.OnClickListener(){
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mFalseButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 checkAnswer(false);
             }
         });
-        mPrevButton = (Button) findViewById (R.id.prev_button);
-        mPrevButton.setOnClickListener(new View.OnClickListener(){
+        mPrevButton = (Button) findViewById(R.id.prev_button);
+        mPrevButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex - 1) % mQuestionBank.length;
+                mIsCheater = false;
                 prevQuestion();
             }
         });
 
-        mNextButton = (Button)  findViewById (R.id.next_button);
-        mNextButton.setOnClickListener(new View.OnClickListener(){
+        mNextButton = (Button) findViewById(R.id.next_button);
+        mNextButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v){
+            public void onClick(View v) {
                 mCurrentIndex = (mCurrentIndex + 1) % mQuestionBank.length;
+                mIsCheater = false;
                 updateQuestion();
             }
         });
+        mCheatButton = (Button) findViewById(R.id.cheat_button);
+        mCheatButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
+                Intent intent = CheatActivity.newIntent(MainActivity.this, answerIsTrue);
+                startActivityForResult(intent, REQUEST_CODE_CHEAT);
+            }
+        });
         updateQuestion();
-     }
-     @Override
-     public void onStart() {
-         super.onStart();
-         Log.d(TAG,"onStart() called");
-     }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode != Activity.RESULT_OK) {
+            return;
+        }
+        if (requestCode == REQUEST_CODE_CHEAT) {
+            if (data == null) {
+                return;
+            }
+            mIsCheater = CheatActivity.wasAnswerShown(data);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        Log.d(TAG, "onStart() called");
+    }
+
     @Override
     public void onResume() {
         super.onResume();
-        Log.d(TAG,"onResume() called");
+        Log.d(TAG, "onResume() called");
     }
+
     @Override
     public void onPause() {
         super.onPause();
-        Log.d(TAG,"onPause() called");
+        Log.d(TAG, "onPause() called");
     }
+
     @Override
-    public void onSaveInstanceState(Bundle savedInstanceState){
+    public void onSaveInstanceState(Bundle savedInstanceState) {
         super.onSaveInstanceState(savedInstanceState);
-        Log.i(TAG,"onSaveInstanceState");
-        savedInstanceState.putInt(KEY_INDEX,mCurrentIndex);
-        }
+        Log.i(TAG, "onSaveInstanceState");
+        savedInstanceState.putInt(KEY_INDEX, mCurrentIndex);
+    }
+
     @Override
     public void onStop() {
         super.onStop();
-        Log.d(TAG,"onStop() called");
+        Log.d(TAG, "onStop() called");
     }
+
     @Override
     public void onDestroy() {
         super.onDestroy();
-        Log.d(TAG,"onDestroy() called");
+        Log.d(TAG, "onDestroy() called");
     }
 
     private void prevQuestion() {
@@ -116,21 +151,24 @@ public class MainActivity extends AppCompatActivity {
         mQuestionTextView.setText(question);
     }
 
-    private void updateQuestion(){
+    private void updateQuestion() {
         int question = mQuestionBank[mCurrentIndex].getTestResId();         //设置问题引用问题数组的id
         mQuestionTextView.setText(question);
-     }
-     private void checkAnswer( boolean userPressedTrue ) {                 //检查答案
+    }
+
+    private void checkAnswer(boolean userPressedTrue) {                 //检查答案
         boolean answerIsTrue = mQuestionBank[mCurrentIndex].isAnswerTrue();
-
-         int messageResId = 0;
-         if (userPressedTrue == answerIsTrue){
-            messageResId = R.string.correct_toast;
+        int messageResId = 0;
+        if (mIsCheater) {
+            messageResId = R.string.judgment_toast;
+            }
+        else {
+            if (userPressedTrue == answerIsTrue) {
+                messageResId = R.string.correct_toast;
+            } else {
+                messageResId = R.string.incorrect_toast;
+            }
+            Toast.makeText(this, messageResId, Toast.LENGTH_SHORT).show();
         }
-        else{
-            messageResId = R.string.incorrect_toast;
-        }
-        Toast.makeText(this,messageResId,Toast.LENGTH_SHORT).show();
-
-     }
+    }
 }

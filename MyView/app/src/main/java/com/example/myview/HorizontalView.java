838dc4ca635cdafcba953389d009ecb894943c63
刘昱欣 <2590177089@qq.com>
2021-10-14 19:44:44 +0800

    hV

--- MyView/app/src/main/java/com/example/myview/HorizontalView.java
+++ MyView/app/src/main/java/com/example/myview/HorizontalView.java
@@ -1,24 +1,42 @@
 package com.example.myview;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.os.Build;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
 import android.view.ViewGroup;
-
-import androidx.annotation.RequiresApi;
+import android.widget.Scroller;
 
 public class HorizontalView extends ViewGroup {
+
+    private int lastInterceptX, lastInterceptY;
+    private int lastX, lastY;
+    int currentIndex = 0;
+    int childWidth = 0;
+    private Scroller scroller;
+    private VelocityTracker tracker;
+
+    public void init(){
+        scroller = new Scroller(getContext());
+        tracker = VelocityTracker.obtain();
+    }
+
     public HorizontalView(Context context) {
         super(context);
+        init();
     }
 
     public HorizontalView(Context context, AttributeSet attrs) {
         super(context, attrs);
+        init();
     }
 
     public HorizontalView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        init();
     }
 
     @Override
@@ -68,4 +86,102 @@ public class HorizontalView extends ViewGroup {
             }
         }
     }
+    //处理滑动冲突
+    @Override
+    public boolean onInterceptTouchEvent (MotionEvent event) {
+        boolean intercept = false;
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                intercept = false;
+                Log.d("this","false");
+                if(!scroller.isFinished()) {
+                    scroller.abortAnimation();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - lastInterceptX;
+                int deltaY = y - lastInterceptY;
+                //viewGroup拦截水平滑动
+                if(Math.abs(deltaX) - Math.abs(deltaY) > 0){
+                    intercept = true;
+                    Log.d("this","true");
+                }else{
+                    intercept = false;
+                    Log.d("this","false");
+                }
+                break;
+            case MotionEvent.ACTION_UP:
+                intercept = false;
+                break;
+        }
+        lastX = x;
+        lastY = y;
+        lastInterceptX = x;
+        lastInterceptY = y;
+        return intercept;
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        tracker.addMovement(event);
+        int x = (int) event.getX();
+        int y = (int) event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                if(!scroller.isFinished()){
+                    scroller.abortAnimation();
+                }
+                break;
+            case MotionEvent.ACTION_MOVE:
+                int deltaX = x - lastX;
+                scrollBy(-deltaX, 0);
+                break;
+            case MotionEvent.ACTION_UP:
+                int distance = getScrollX() - currentIndex * childWidth;
+                if(Math.abs(distance) > childWidth / 2){   //滑动的距离是否大于宽度的1/2
+                    if(distance > 0){
+                        currentIndex++;
+                    }else {
+                        currentIndex--;
+                    }
+                }else{
+                    tracker.computeCurrentVelocity(1000);//获取水平方向的速度
+                    float xV = tracker.getXVelocity();
+                    if(Math.abs(xV) > 50){ //速度绝对值大于50将被认为是快速滑动
+                        if(xV > 0){
+                            currentIndex--;
+                        }else{
+                            currentIndex++;
+                        }
+                    }
+                }
+                currentIndex = currentIndex < 0 ? 0 : Math.min(currentIndex, getChildCount()
+                        - 1);
+                childWidth = getChildAt(0).getWidth();
+                smoothScrollTo(currentIndex * childWidth, 0);
+                tracker.clear(); //重置速度计算器
+                break;
+        }
+        lastX = x;
+        lastY = y;
+        return true;
+    }
+
+    @Override
+    public void computeScroll() {
+        super.computeScroll();
+        if(scroller.computeScrollOffset()) {
+            scrollTo(scroller.getCurrX(), scroller.getCurrY());
+            postInvalidate();
+        }
+    }
+    //弹性滑动到指定位置
+    public void smoothScrollTo(int destX, int destY){
+        scroller.startScroll(getScrollX(), getScrollY(), destX - getScrollX(), destY - getScrollY(), 1000);
+        invalidate();
+    }
+
 }
--- MyView/app/src/main/java/com/example/myview/MainActivity.java
+++ MyView/app/src/main/java/com/example/myview/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.myview;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private RectView mView;
+    private ListView lv_one,lv_two;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.horizontal_view);
+        lv_one = findViewById(R.id.lv_one);
+        lv_two = findViewById(R.id.lv_two);
+        String[] str1 = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"};
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>
+                (this, android.R.layout.simple_expandable_list_item_1,str1);
+        lv_one.setAdapter(adapter1);
         //mView = findViewById(R.id.rv_rect);
        // mView.setOnClickListener(v -> mView.setAlpha((float) 0.5));
+        String[] str2 = {"A","B","C","D","E","F","G","H","I","J","K","L","M","N","O"};
+        ArrayAdapter<String> adapter2 = new ArrayAdapter<String>
+                (this, android.R.layout.simple_expandable_list_item_1,str2);
+        lv_two.setAdapter(adapter2);
     }
-
-
 }
